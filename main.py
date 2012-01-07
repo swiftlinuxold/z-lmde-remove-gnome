634b0a3856efcdfe7fe31d43e3e98920978753be
Jason Hsu <github@jasonhsu.com>
2012-01-07 01:28:10 -0600

    Started adding packages to purge

--- main.py
+++ main.py
@@ -27,8 +27,45 @@ else:
 print '============================='
 print 'BEGIN REMOVING GNOME PACKAGES'
 
-# Remove GNOME
-# os.system('apt-get remove -y')
+# Remove GNOME Desktop Environment (universe) and dependants
+os.system('apt-get purge -y mint-x-icons mint-x-theme mint-artwork-debian')
+
+# Remove GNOME Desktop Environment and dependants
+# To see the list of installed packages, open Synaptic and go to Sections -> 
+# GNOME Desktop Environment and sort by the first column.
+os.system('apt-get purge -y brasero banshee nautilus')
+os.system('apt-get purge -y brasero-common brasero libbrasero-media0')
+os.system('apt-get purge -y eog')
+os.system('apt-get purge -y evince evince-common')
+os.system('apt-get purge -y gnome-applets gnome-panel')  
+os.system('apt-get purge -y gnome-applets-data') 
+os.system('apt-get purge -y gnome-bluetooth gnome-user-share network-manager-gnome nautilus-sendto') 
+# Keeping gnome-icon-theme due to dependants
+os.system('apt-get purge -y gnome-nettool') 
+os.system('apt-get purge -y gnome-power-manager') 
+os.system('apt-get purge -y gnome-settings-daemon') 
+os.system('apt-get purge -y libevolution') 
+# Keeping libreoffice-gnome, libreoffice-gtk, and policykit-1-gnome due to dependants
+# Keeping simple-scan
+os.system('apt-get purge -y vino') 
+# Keeping zenity for its use in some scripts
+# Keeping at-spi for its utility for the disabled and its light footprint
+# Keeping baobab (graphical display of disk space usage)
+os.system('apt-get purge -y capplets-data') 
+# Keeping cups-pk-helper due to need for CUPS
+os.system('apt-get purge -y deskbar-applet mintmenu mint-meta-common') 
+os.system('apt-get purge -y dmz-cursor-theme') 
+os.system('apt-get purge -y evolution-data-server python-evolution pidgin') 
+os.system('apt-get purge -y evolution-data-server-common libedataserverui1.2-8 libedataserverui1.2-11') 
+# Keeping file-roller for working with *.zip, *.tar, and other archives
+os.system('apt-get purge -y gedit gedit-common') 
+
+
+      
+      
+      
+      
+
 
 
 print 'FINISHED REMOVING GNOME PACKAGES'
